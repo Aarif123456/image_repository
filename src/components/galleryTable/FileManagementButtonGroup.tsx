fc9a4831e3938c96149e98447cd79bd24ee76a7e
Abdullah Arif <arif115@uwindsor.ca>
2021-05-14 19:42:59 -0400

    made downlaod icon normal instead of sharp

--- src/components/galleryTable/FileManagementButtonGroup.tsx
+++ src/components/galleryTable/FileManagementButtonGroup.tsx
@@ -2,7 +2,8 @@ import { FunctionComponent } from 'react';
 import { TableData } from '.';
 import { useIntl } from 'react-intl';
 import { IconButton, ButtonGroup } from '@material-ui/core';
-import CloudDownloadSharpIcon from '@material-ui/icons/CloudDownloadSharp';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export const FileManagementButtonGroup: FunctionComponent<TableData> = ({ fileName }) => {
     /* TODO: make button call endpoint that triggers a download*/
@@ -16,7 +17,16 @@ export const FileManagementButtonGroup: FunctionComponent<TableData> = ({ fileNa
                 aria-label={intl.formatMessage({
                     id: 'FileManagement.download'
                 })}>
-                <CloudDownloadSharpIcon />
+                <CloudDownloadIcon />
+            </IconButton>
+            <IconButton
+                onClick={() => {
+                    alert(`Will trigger a download of the ${fileName} file`);
+                }}
+                aria-label={intl.formatMessage({
+                    id: 'FileManagement.delete'
+                })}>
+                <DeleteIcon />
             </IconButton>
         </ButtonGroup>
     );
