2b9944244a7b8c5992aa292f4d4f4db8fed225e8
aarif123456 <abdullahmeo11@gmail.com>
2021-05-25 22:02:18 -0400

    seperated endpoint for testing

--- src/common/dataRetrieval/apiConstants.ts
+++ src/common/dataRetrieval/apiConstants.ts
@@ -1,6 +1,16 @@
 import axios, { AxiosInstance } from 'axios';
 
-export const API = 'https://arif115.myweb.cs.uwindsor.ca/imagerepository/api/';
+function getAPI() {
+    switch (process.env.NODE_ENV) {
+        case 'development':
+            return 'http://localhost:4000';
+        case 'test':
+            return 'http://localhost:4000';
+        case 'production':
+            return 'https://arif115.myweb.cs.uwindsor.ca/imagerepository/api/';
+    }
+}
+export const API = getAPI();
 
 export const AXIOS_ENDPOINT: AxiosInstance = axios.create({
     withCredentials: true,
